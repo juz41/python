ad1e544e6a851463f15c89b79b731d98b561b8eb
juz41 <98811419+juz41@users.noreply.github.com>
2022-02-18 17:33:50 +0100

    Add files via upload

--- gradient.py
+++ gradient.py
@@ -1,23 +1,37 @@
+from math import sqrt
+from math import ceil
 import random
 from PIL import Image
 
-sizex=10000
-sizey=1000
+sizex=500
+sizey=500
 img=Image.new( mode = "RGB", size = (sizex, sizey), color=(255,255,255))
-path="C:\\Users\\Julek\\Documents\\Coding\\Python\\gradient\\"
+path="/home/synch/Documents/Coding/python"
 
-start=[255,0,255]
-end=[255,255,112]
-diff=[]
-for i in range(3):
-    diff.append(end[i]-start[i])
-    diff[i]/=sizex
-
-#print (diff)
-
+points=[ [[0,0],[255,0,0]],[[sizex-1,sizey-1],[0,255,0]],[[200,200],[0,0,255]]]
+numberOfPoints=3
 for i in range(sizex):
     for j in range(sizey):
-        img.putpixel ( (i,j), ( round(start[0]+diff[0]*i),round(start[1]+diff[1]*i),round(start[2]+diff[2]*i) ) )
+        r=0
+        g=0
+        b=0
+        dis=[]
+        disSum=0
+        for z in range(numberOfPoints):
+            dis.append(0)
+
+        for z in range(numberOfPoints):
+            dis[z]=sqrt( (points[z][0][0]-i)**2+(points[z][0][1]-j)**2 )
+            disSum+=dis[z]
+
+        for z in range(numberOfPoints):
+            r+=(dis[z]/disSum)*points[z][1][0]
+            g+=(dis[z]/disSum)*points[z][1][1]
+            b+=(dis[z]/disSum)*points[z][1][2]
+        r=ceil(r)
+        g=ceil(g)
+        b=ceil(b)
+        img.putpixel ((i,j), (r, g, b))
 
 img.save(path+'gradient.png', format="PNG")
 
